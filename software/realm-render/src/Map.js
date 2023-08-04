7c09da0212f5ea0ab26047610e3c11ea5bb6dfac
Nathan Collinson <collinnath1@ecs.vuw.ac.nz>
2023-08-04 15:39:23 +1200

    create the initial classes for controller, game and map

---
+++ software/realm-render/src/Map.js
@@ -0,0 +1,14 @@
+/**
+ * Map will represent the current dungeon map.
+ * The dungeon map will be a 2D array of tiles.
+ * There are different rooms and hallways.
+ * and each tile will have a type with various items possiblys
+ * 
+ */
+
+var Map = function() {
+    this.tiles = [];
+    this.width = 0;
+    this.height = 0;
+};
+
