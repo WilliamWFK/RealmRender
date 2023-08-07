fbb99e762d093b8287bb233f5e2efef03886a895
holdawmatt <holdawmatt@ecs.vuw.ac.nz>
2023-08-07 16:00:54 +1200

    Implemented Draft landing page by editing App.js & App.css

--- software/realm-render/src/App.js
+++ software/realm-render/src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './styles/App.css';
+import background from './RealmRenderWireframe1.drawio.svg';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,7 +18,16 @@ function App() {
         >
           Learn React
         </a>
+      </header> */}
+      <header className="App-header">
+        Realm Render
       </header>
+      <div className="menuButtons">
+        <a href="#" className="menuButtonItem"><p>New</p></a>
+        <a href="#" className="menuButtonItem"><p>Load</p></a>
+        <a href="#" className="menuButtonItem"><p>Join</p></a>
+      </div>
+      <img src={background}></img>
     </div>
   );
 }
