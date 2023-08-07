c74bfe494ee2d2d32c0b51525f5be80abc3bc19d
Nathan Collinson <collinnath1@ecs.vuw.ac.nz>
2023-08-07 13:33:41 +1200

    Create more classes and folder structure

---
+++ software/realm-render/src/Model/Room.js
@@ -0,0 +1,10 @@
+class Room {
+    constructor() {
+        this.id = 0;
+        this.name = "";
+        this.description = "";
+        this.exits = [];
+        this.items = [];
+        this.characters = [];
+    }
+}
