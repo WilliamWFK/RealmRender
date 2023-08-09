f322c2fb8f539f4415c15cd38888b40d81017dbb
MattyMan03 <matthewholdaway03@gmail.com>
2023-08-09 18:40:48 +1200

    refactor(website): Removed commented out code & Updated favicon/title

--- software/realm-render/src/App.js
+++ software/realm-render/src/App.js
@@ -1,24 +1,9 @@
-import logo from './logo.svg';
 import './styles/App.css';
 import background from './RealmRenderWireframe1.drawio.svg';
 
 function App() {
   return (
     <div className="App">
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
       <header className="App-header">
         Realm Render
       </header>
@@ -27,7 +12,7 @@ function App() {
         <a href="#" className="menuButtonItem"><p>Load</p></a>
         <a href="#" className="menuButtonItem"><p>Join</p></a>
       </div>
-      <img src={background}></img>
+      <img src={background} alt="background"></img>
     </div>
   );
 }
