79726c7ab41bb5a7e24523f490ae93a7d7082e6d
Leonard <holmesleon@myvuw.ac.nz>
2023-08-13 06:22:11 +1200

    feat(software): managed to create tiles that appear in grind with basic imgs

--- software/realm-render/src/App.js
+++ software/realm-render/src/App.js
@@ -1,6 +1,37 @@
 import './styles/App.css';
 import background from './RealmRenderWireframe1.drawio.svg';
+import React from 'react';
+import Tile from './Model/Tile';
+import TileComponent from './Model/TileComponent';
 
+const createGrid = () => {
+  const grid = [];
+  for (let x = 0; x < 10; x++) {
+    for (let y = 0; y < 10; y++) {
+      const tile = new Tile(x, y, 0, "grass", "nothing", "TilesImg/Allwalls.png");
+      grid.push(tile);
+    }
+  }
+  return grid;
+};
+
+const App = () => {
+  const grid = createGrid();
+
+  return (
+    <div className="App">
+      <div className="map-container">
+        {grid.map((tile, index) => (
+          <TileComponent key={index} tile={tile} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default App;
+
+/** 
 function App() {
   return (
     <div className="App">
@@ -18,3 +49,4 @@ function App() {
 }
 
 export default App;
+*/
--- software/realm-render/src/Model/Tile.js
+++ software/realm-render/src/Model/Tile.js
@@ -1,8 +1,18 @@
 class Tile {
-    constructor(x, y, z, type) {
+    constructor(x, y, z, type, contents, image) {
         this.x = x;
         this.y = y;
         this.z = z;
-        this.type = type;
+        this.type = type;       // Thematic type of tile
+        this.contents = contents; // What's on the tile
+        this.directions = { //to tell whats edges are made of
+            up: "wall",
+            right: "empty",
+            down: "wall",
+            left: "empty"
+        };
+        this.image = image;
     }
 }
+
+export default Tile;
