4223be8fbc5cf4e719c1b70a5cae7c68d763a838
Nathan Collinson <collinnath1@ecs.vuw.ac.nz>
2023-08-15 15:04:29 +1200

    refactor(software): create navigation and load map page

---
+++ software/realm-render/src/components/Navbar/index.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { Nav, NavLink, NavMenu }
+    from "./NavbarElements";
+ 
+const Navbar = () => {
+    return (
+        <>
+            <Nav>
+                <NavMenu>
+                    <NavLink to="/loadMap" activeStyle>
+                        LoadMap
+                    </NavLink>
+                </NavMenu>
+            </Nav>
+        </>
+    );
+};
+ 
+export default Navbar;
