0efef48624acd8e8a6485e8b41d19766d63deac7
Nathan Collinson <collinnath1@ecs.vuw.ac.nz>
2023-08-17 19:05:40 +1200

    fix(Software): Fix spelling mistake

--- software/realm-render/src/pages/loadMap.js
+++ software/realm-render/src/pages/loadMap.js
@@ -2,10 +2,10 @@ import React from "react";
 import { Link } from 'react-router-dom';
 
 const LoadMap = () => {
-  return (
-    <div>
-      <h1>
-                LoadMap
+    return (
+        <div>
+            <h1>
+                Load Map
             </h1>
             <Link to="/index" className="menuButtonItem"><p>Home</p></Link>
         </div>
