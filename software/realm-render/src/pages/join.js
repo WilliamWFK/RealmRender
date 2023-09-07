1ac87e311dfe8dcffe6d5e297721c4076d83fb09
MattyMan03 <matthewholdaway03@gmail.com>
2023-09-08 00:32:37 +1200

    feat(web): Added navigation for webpages, changed styling & added join.js

---
+++ software/realm-render/src/pages/join.js
@@ -0,0 +1,13 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+
+const Join = () => {
+    return (
+        <div class="App">
+            <h1>Feature not implemented yet</h1>
+            <Link to="/index" class="backButton">Back</Link>
+        </div>
+    )
+}
+
+export default Join;
