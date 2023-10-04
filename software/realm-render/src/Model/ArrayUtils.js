bc4f4e5f963ede73f246620a3491d91a712d2c74
Nathan Collinson <collinnath1@ecs.vuw.ac.nz>
2023-10-04 15:13:08 +1300

    feat(software): add function to put decoration on floor tiles

--- software/realm-render/src/Model/ArrayUtils.js
+++ software/realm-render/src/Model/ArrayUtils.js
@@ -12,3 +12,26 @@ export function rotate2DArray(array) {
 
     return rotatedArray;
 }
+
+//Function to decorate floor tiles with objects
+export function decorateFloorTiles(array) {
+    const numRows = array.length;
+    const numCols = array[0].length;
+    const decoratedArray = [...array];
+
+    for(let col = 1; col < numCols-1; col++){
+        for (let row = 1; row < numRows-1; row++) {
+            if(array[col][row] === "floor"){
+                //check for adjacent walls
+                //if adjacent walls, add decoration
+                if(array[col][row+1] === "wall" || array[col][row-1] === "wall" || array[col+1][row] === "wall" || array[col-1][row] === "wall"){
+                    let rand = Math.floor(Math.random()*100) +1;
+                    if(rand <= 30) decoratedArray[col][row].setType("object");
+                    else if(rand <= 40) decoratedArray[col][row].setType("chest");
+                }
+
+            }
+        }
+    }
+    return decoratedArray;
+}
