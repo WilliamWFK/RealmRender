e0a3cfb9edd32400ba355da9198a79f0cf2d75fc
Nathan Kaffes <nathan@Nathans-MacBook-Pro.local>
2023-10-05 16:34:51 +1300

    feat(p5js): rebased on main, ensured p5js compatible with changes

--- software/realm-render/src/App.js
+++ software/realm-render/src/App.js
@@ -18,7 +18,6 @@ function App() {
         <Route path='/mapEditor' element={<MapEditor />} />
         <Route path='/index' element={<Home />} />
         <Route path='/join' element={<Join />} />
-        <Route path='/createMap' element={<CreateMap />} /> 
       </Routes>
     </Router>
   );
--- software/realm-render/src/pages/index.js
+++ software/realm-render/src/pages/index.js
@@ -13,7 +13,7 @@ const Home = () => {
       const { name, value } = event.target;
       setFormData((prevFormData) => ({ ...prevFormData, [name]: value }));
   };
-  
+
   const navigate = useNavigate();
 
   const handleSubmit = (event) => {
@@ -21,7 +21,7 @@ const Home = () => {
       alert(`Name: ${formData.name}, Email: ${formData.width}, Message: ${formData.height}, Players: ${formData.players}, Theme: ${formData.theme}`
       );
 
-      navigate("/createMap", {state: {name: formData.name, width: formData.width, height: formData.height, players: formData.players, theme: formData.theme}});
+      navigate("/mapEditor", {state: {name: formData.name, width: formData.width, height: formData.height, players: formData.players, theme: formData.theme}});
   }
 
   function menuContents() {
@@ -75,15 +75,15 @@ const Home = () => {
                   <input type="range" min="1" max="6" defaultValue={formData.players} onChange={(e) =>setFormData({ ...formData, players: parseInt(e.target.value) })} id="players" name="players"></input>
                 </div>
 
-                
-                
+
+
                 <div class="navButtons">
                   <div class="backButton" onClick={() => { setMenumode(0) }}><p class="caret">&lt;</p><p class="text">Back</p></div>
                   <button type="submit" class="createButton"><p class="text">Create</p><p class="plus">+</p></button>
                 </div>
               </form>
             </div>
-            
+
           </div>
           <div class="menuBackdrop mainMenu">
             <div onClick={() => { setMenumode(1) }} class="menuButtonItem"><p>New</p></div>
