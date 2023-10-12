dc205deb271f8bdb39e2bdb3cef63113793c0b8c
Will <42400351+WilliamWFK@users.noreply.github.com>
2023-10-13 02:40:37 +1300

    feat(RTDB): Added rtdb model, renamed join to playerView

---
+++ software/realm-render/src/firebaseConfig.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBf7nypCQyf67H0DHd6GkefFjjhJ6T5JWw",
+    authDomain: "realm-render-22cc4.firebaseapp.com",
+    projectId: "realm-render-22cc4",
+    storageBucket: "realm-render-22cc4.appspot.com",
+    messagingSenderId: "982930052324",
+    appId: "1:982930052324:web:1f51f59a9ea733ec4308f1"
+  };
+
+  export default firebaseConfig;
---
+++ software/realm-render/src/pages/playerView.js
@@ -0,0 +1,15 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+
+
+const Join = () => {
+    return (
+        <div class="App">
+            <h1>Feature not implemented yet</h1>
+            <h2>William is working on this now!</h2>
+            <Link to="/index" class="backButton">Back</Link>
+        </div>
+    )
+}
+
+export default Join;
